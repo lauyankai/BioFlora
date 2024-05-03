5ca3137b6740e15998fc72aa0f90fb8c77b90ee7
lauyankai <lauyankai04@gmail.com>
2024-05-03 16:43:18 +0800

    chore: Update site title and user information

--- src/consts.ts
+++ src/consts.ts
@@ -1,8 +1,7 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
+
+export const SITE_TITLE = 'BioFlora';
 export const SITE_DESCRIPTION = 'Welcome to my website!';
-export const USER_NICKNAME = 'EveSunMaple';
-export const USER_DESCRIPTION = 'a student';
+export const USER_NICKNAME = 'Group 6';
+export const USER_DESCRIPTION = 'A group of Student from Section 2 of the course HCI!';
 export const TRANSITION_API = true;
